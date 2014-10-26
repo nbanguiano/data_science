2707f1215109fa772cad104892546cedfebaad02
Nicolas Anguiano <nicolas.anguiano@gmail.com>
2014-10-26 02:33:42 +0200

    completed programming assignment on multi-classifiers and neural networks

--- coursera/machine_learning/mlclass-ex3/lrCostFunction.m
+++ coursera/machine_learning/mlclass-ex3/lrCostFunction.m
@@ -1,9 +1,6 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
-%regularization
-%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
-%   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+% LRCOSTFUNCTION Compute cost and gradient for logistic
+% regression with regularization
 
 % Initialize some useful values
 m = length(y); % number of training examples
@@ -14,38 +11,39 @@ grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
+% You should set J to the cost.
+% Compute the partial derivatives and set grad to the partial
+% Hint: The computation of the cost function and gradients can
+% be efficiently vectorized. For example, consider the
+% computation
 %
 %           sigmoid(X * theta)
 %
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+% Each row of the resulting matrix will contain the value of the prediction for that example. You can make use of this to vectorize the cost function and gradient computations. 
 %
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+% Hint: When computing the gradient of the regularized cost function, there're many possible vectorized solutions, but one solution looks like:
 %
+% grad = (unregularized gradient for logistic regression)
+% 	temp = theta; 
+% 	temp(1) = 0;   % because we don't add anything for j = 0  
+% 	grad = grad + YOUR_CODE_HERE (using the temp variable)
 
+h = sigmoid(X*theta);
 
+initial_cost = (-(y'*log(h) + (1-y)'*log(1-h)) / m);
+reg_cost = (lambda * sum(theta(2:end).^2)/(2*m));
 
+J = initial_cost + reg_cost;
 
+% Depending on the dimensions of the vectors below, you need to 
+% add or dot product the gradient and the regularization
 
+initial_grad = ((h-y)' * X) / m;	
+reg_grad = (lambda * theta(2:end)/m);
 
+grad = initial_grad + [0 ; reg_grad]';
 
-
-
-
-% =============================================================
+% ============================================================
 
 grad = grad(:);
 
